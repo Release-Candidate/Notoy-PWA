c0054b8a372c7c8aa96f373b16ca10374c821010
Release-Candidate <rec@gmx.at>
2021-12-20 22:06:15 +0100

    Let parcel generate the list of chached items

--- src/service_worker/sw.js
+++ src/service_worker/sw.js
@@ -6,14 +6,38 @@
 // Date:     02.Dec.2021
 //
 // ==============================================================================
+import { manifest, version } from "@parcel/service-worker"
 
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open("airhorner").then((cache) => cache.addAll([
-            "./",
-            "./index.html",
-            "./dist/app.js",
-            "./dist/app.css",
-        ]))
+async function install() {
+    const cache = await caches.open(version)
+    await cache.addAll(manifest)
+    console.log(`[Service Worker] installed`)
+}
+addEventListener("install", (e) => e.waitUntil(install()))
+
+async function activate() {
+    const keys = await caches.keys()
+    await Promise.all(keys.map((key) => key !== version && caches.delete(key)))
+    console.log(`[Service Worker] activated`)
+}
+addEventListener("activate", (e) => e.waitUntil(activate()))
+
+addEventListener("fetch", (e) => {
+    console.log(`[Service Worker] fetch`)
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request)
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
+            if (r) {
+                return r
+            }
+            const response = await fetch(e.request)
+            const cache = await caches.open(version)
+            console.log(
+                `[Service Worker] Caching new resource: ${e.request.url}`
+            )
+            cache.put(e.request, response.clone())
+            return response
+        })()
     )
 })

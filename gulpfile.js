0a347200cfd4b5c0ef05d7622ce007a966fdd59e
Release-Candidate <rec@gmx.at>
2021-12-21 18:45:07 +0100

    Use gulp-connect as HTTPS server

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,44 @@
 // ==============================================================================
 /* eslint-disable no-console */
 
+//==============================================================================
+// Directories and files.
+
+// Path to HTTPS certificate and key
+const httpsCertificate = "../https_cert.pem"
+const httpsCertificateKey = "../https_cert-key.pem"
+
+// HTTPS port to use
+const httpsPort = 1234
+
+// Service worker, JS file.
+const serviceWorkerJS = "./src/service_worker/sw.js"
+
+// App JS file.
+const appJS = "app.js"
+
+// Manifest filename (original).
+const manifestJSON = "manifest.json"
+
+// Changelog file.
+const changelogPath = "./CHANGELOG.md"
+
+// Directory holding the assets.
+const assetDir = "./assets"
+
+// Source directory for the bundler, copy every source to this dir.
+const outDir = "./http"
+
+// Output directory of compiled PureScript files.
+const spagoOutdir = "./output"
+
+// Destination directory of the bundler, the source directory for the HTTPS
+// Server.
+const serveDir = "./dist"
+
+//==============================================================================
+// JS requires
+
 // eslint-disable-next-line no-undef
 const { series, parallel } = require("gulp")
 
@@ -24,6 +62,9 @@ const del = require("delete")
 const replace = require("gulp-string-replace")
 
 // eslint-disable-next-line no-undef
+const connect = require("gulp-connect")
+
+// eslint-disable-next-line no-undef
 const fs = require("fs")
 
 //==============================================================================
@@ -32,7 +73,7 @@ const fs = require("fs")
 function scanChangelogVersion() {
     let version = ""
     try {
-        const data = fs.readFileSync("./CHANGELOG.md", "utf8")
+        const data = fs.readFileSync(changelogPath, "utf8")
         const match = data
             .toString()
             .match(/##\s+Version\s+(?<versionMatch>[0-9]+.[0-9]+.[0-9]+)/u)
@@ -46,7 +87,7 @@ function scanChangelogVersion() {
 }
 
 function replaceVersion(dirName, version) {
-    return src("./" + dirName + "/manifest.json")
+    return src(dirName + "/" + manifestJSON)
         .pipe(
             replace(
                 /"version":\s+"[0-9]+.[0-9]+.[0-9]+",/gu,
@@ -56,15 +97,15 @@ function replaceVersion(dirName, version) {
         .pipe(dest("./" + dirName))
 }
 
-function replaceVersionAssets() {
-    return replaceVersion("assets", scanChangelogVersion())
+function replaceVersionOutdir() {
+    return replaceVersion(outDir, scanChangelogVersion())
 }
 
 //==============================================================================
 // Run spago bundle.
 function runSpago() {
     return exec(
-        "spago bundle-app --main Main --to output/app.js",
+        `spago bundle-app --main Main --to ${spagoOutdir}/${appJS}`,
         (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`)
@@ -77,22 +118,24 @@ function runSpago() {
 }
 
 //==============================================================================
-// Start HTTP server.
-function runHTTP() {
-    return exec("parcel --open --https", (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`)
-            return
-        }
-        console.log(`stdout: ${stdout}`)
-        console.log(`stderr: ${stderr}`)
+// Start HTTPS server.
+function runHTTPS() {
+    connect.server({
+        root: serveDir,
+        https: {
+            key: fs.readFileSync(httpsCertificateKey),
+            cert: fs.readFileSync(httpsCertificate),
+        },
+        host: "0.0.0.0",
+        livereload: true,
+        port: httpsPort,
     })
 }
 
 //==============================================================================
 // Run parcel-bundle on `index.html`.
 function runParcel() {
-    return exec("parcel --https", (error, stdout, stderr) => {
+    return exec("parcel", (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`)
             return
@@ -103,29 +146,47 @@ function runParcel() {
 }
 
 //==============================================================================
+// Copy service worker to ./http
+function copyServiceWorker() {
+    return src(serviceWorkerJS).pipe(dest(outDir))
+}
+
+//==============================================================================
+// Copy everything in ./assets to ./http
+function copyAssets() {
+    return src(assetDir + "/**/*").pipe(dest(outDir))
+}
+
+//==============================================================================
 // Delete generated files.
 function delDirectory(dirName, cb) {
     del([dirName], cb)
 }
 
 function cleanDist(cb) {
-    delDirectory("./dist/", cb)
+    delDirectory(serveDir, cb)
+    cb()
+}
+
+function cleanHTTP(cb) {
+    delDirectory(outDir, cb)
     cb()
 }
 
 function cleanOutput(cb) {
-    delDirectory("./output/", cb)
+    delDirectory(spagoOutdir, cb)
     cb()
 }
 
 // eslint-disable-next-line no-undef
-exports.clean = parallel(cleanDist, cleanOutput)
+exports.clean = parallel(cleanDist, cleanOutput, cleanHTTP)
 
 // eslint-disable-next-line no-undef
-exports.bundle = parallel(series(runSpago, runParcel), replaceVersionAssets)
-
-// eslint-disable-next-line no-undef
-exports.serve = parallel(
-    series(runSpago, runParcel, runHTTP),
-    replaceVersionAssets
+exports.bundle = parallel(
+    series(runSpago, runParcel),
+    copyServiceWorker,
+    series(copyAssets, replaceVersionOutdir)
 )
+
+// eslint-disable-next-line no-undef
+exports.serve = series(runHTTPS)

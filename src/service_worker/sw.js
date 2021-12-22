a3147ce39a4b19cbcdf5b52ff6ba0944e06fe628
Release-Candidate <rec@gmx.at>
2021-12-22 22:05:18 +0100

    Document service worker

--- src/service_worker/sw.js
+++ src/service_worker/sw.js
@@ -6,38 +6,68 @@
 // Date:     02.Dec.2021
 //
 // ==============================================================================
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+
+// `version` is the name of the cache, a hash.
+// `manifest` is an array holding the paths to all files to cache.
 import { manifest, version } from "@parcel/service-worker"
 
+//==============================================================================
+// Installation
+
+/**
+ * Install the service worker.
+ * On installation, all files Parcel knows about are added to the cache.
+ */
 async function install() {
     const cache = await caches.open(version)
     await cache.addAll(manifest)
-    console.log(`[Service Worker] installed`)
+    // eslint-disable-next-line no-console
+    console.log(`[Service Worker] installed files to ${version}`)
 }
-addEventListener("install", (e) => e.waitUntil(install()))
 
+addEventListener("install", (event) => event.waitUntil(install()))
+
+//==============================================================================
+// Activation
+
+/**
+ * Activate service worker.
+ * On activation all files from older versions of the cache are deleted.
+ */
 async function activate() {
     const keys = await caches.keys()
     await Promise.all(keys.map((key) => key !== version && caches.delete(key)))
+    // eslint-disable-next-line no-console
     console.log(`[Service Worker] activated`)
 }
-addEventListener("activate", (e) => e.waitUntil(activate()))
 
-addEventListener("fetch", (e) => {
-    console.log(`[Service Worker] fetch`)
-    e.respondWith(
-        (async () => {
-            const r = await caches.match(e.request)
-            if (r) {
-                console.log(`[Service Worker] Cache hit: ${e.request.url}`)
-                return r
-            }
-            const response = await fetch(e.request)
-            const cache = await caches.open(version)
-            console.log(
-                `[Service Worker] Caching new resource: ${e.request.url}`
-            )
-            cache.put(e.request, response.clone())
-            return response
-        })()
-    )
-})
+addEventListener("activate", (event) => event.waitUntil(activate()))
+
+//==============================================================================
+// Fetching
+
+/**
+ * Fetches the given URL, either from cache or the server.
+ *
+ * @param {RequestInfo} request The request to fulfill.
+ *
+ * @returns {Response} The fetched URL as `Response`.
+ */
+async function fetchFromCache(request) {
+    const cachedResponse = await caches.match(request)
+    if (cachedResponse) {
+        // eslint-disable-next-line no-console
+        console.log(`[Service Worker] cache hit: ${request.url}`)
+        return cachedResponse
+    }
+    // eslint-disable-next-line no-console
+    console.log(`[Service Worker] fetching ${request.url}`)
+    const response = await fetch(request)
+    return response
+}
+
+addEventListener("fetch", (event) =>
+    event.respondWith(fetchFromCache(event.request))
+)

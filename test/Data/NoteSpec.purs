03b19a19d0487c6bd4ad7df0d2c4056e449ad61b
Release-Candidate <rec@gmx.at>
2022-01-01 00:13:22 +0100

    Add tests

--- test/Data/NoteSpec.purs
+++ test/Data/NoteSpec.purs
@@ -12,8 +12,13 @@ module Test.Data.NoteSpec
   ) where
 
 import Prelude
+import Data.Foldable (for_)
+import Data.Interpolate (interp)
+import Data.Maybe (Maybe(..))
+import Data.Note (Note(..), fromShared)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
+import Web.URL (fromAbsolute)
 
 {-------------------------------------------------------------------------------
 | The tests to run.
@@ -23,5 +28,53 @@ import Test.Spec.Assertions (shouldEqual)
 spec :: Spec Unit
 spec =
   describe "Data.Note - Tests" do
-    describe "Group 1" do
-      it "No Tests" $ pure unit
+    fromSharedSpecs
+
+fromSharedSpecs :: Spec Unit
+fromSharedSpecs =
+  describe "fromShared" do
+    for_ fromSharedNotes \note@( Note
+        { title: title
+      , url: url
+      , shortDesc: shortDesc
+      , longDesc: _
+      }
+    ) ->
+      it (interp "From Note: " (show note)) do
+        fromShared title url shortDesc
+          `shouldEqual`
+            note
+
+fromSharedNotes :: Array Note
+fromSharedNotes =
+  [ Note
+      { title: Just "Title 1"
+      , url: fromAbsolute "https://url.com:12354/index.html"
+      , shortDesc: Just "Short text"
+      , longDesc: Nothing
+      }
+  , Note
+      { title: Just "Title 2"
+      , url: Nothing
+      , shortDesc: Just "Short text 2"
+      , longDesc: Nothing
+      }
+  , Note
+      { title: Just "Title 3"
+      , url: Nothing
+      , shortDesc: Nothing
+      , longDesc: Nothing
+      }
+  , Note
+      { title: Nothing
+      , url: Nothing
+      , shortDesc: Just "Short text 4"
+      , longDesc: Nothing
+      }
+  , Note
+      { title: Nothing
+      , url: Nothing
+      , shortDesc: Nothing
+      , longDesc: Nothing
+      }
+  ]
